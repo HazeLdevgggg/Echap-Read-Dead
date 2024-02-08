fad9536c99aea36f15b2bd7a4b312b0097d5375f
HazeL <105066838+HazeLdevgggg@users.noreply.github.com>
2024-02-08 20:26:22 +0100

    Update sh_config.lua

--- echap_reddead/lua/autorun/sh_config.lua
+++ echap_reddead/lua/autorun/sh_config.lua
@@ -2,9 +2,9 @@ echap = echap or {}
 
 -- Merci d'avoir acheté mon addon :), voici la config si il ya des problème MP: HazeL#2876
 
-echap.Boutique = "https://discord.gg/gmodfr" --Lien Boutique, pour mettre votre lein changez celui entre ""
-echap.Forum = "https://discord.gg/gmodfr" --Lien Forum, pour mettre votre lein changez celui entre ""
-echap.Discord = "https://discord.gg/gmodfr" --Lien Discord, pour mettre votre lein changez celui entre ""
-echap.Workshop = "https://discord.gg/gmodfr" --Lien Workshop, pour mettre votre lein changez celui entre ""
+echap.Boutique = "https://discord.gg/gmodfr" --Lien Boutique, pour mettre votre Lien changez celui entre ""
+echap.Forum = "https://discord.gg/gmodfr" --Lien Forum, pour mettre votre Lien changez celui entre ""
+echap.Discord = "https://discord.gg/gmodfr" --Lien Discord, pour mettre votre Lien changez celui entre ""
+echap.Workshop = "https://discord.gg/gmodfr" --Lien Workshop, pour mettre votre Lien changez celui entre ""
 
 
